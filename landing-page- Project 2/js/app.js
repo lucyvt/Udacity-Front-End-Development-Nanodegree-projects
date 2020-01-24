f6294a483fb35edf4beb31d81a4eb8d4d8586114
Noha <33437197+Nohaxxxx@users.noreply.github.com>
2020-01-24 15:33:28 +0200

    editing on JS

--- landing-page- Project 2/js/app.js
+++ landing-page- Project 2/js/app.js
@@ -18,40 +18,119 @@
  * 
 */
 
+const sections = document.querySelectorAll('[data-nav]');
+const navList = document.querySelector('#navbar__list');
+
+let newLink;
 
 /**
  * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
+ 
  * Begin Main Functions
  * 
 */
 
+function navbarFilling() {
+	
+	
+	for(let i = 0; i < sections.length; i++) {
+		
+		
+		let navItem = document.createElement('LI');
+	
+		navList.append(navItem);
+	
+		newLink = document.createElement('A')
+	
+		navItem.append(newLink);
+		
+		// use data-nav attribute to fill li node
+		newLink.innerHTML = sections[i].getAttribute('data-nav');
+		
+		newLink.classList.add('menu__link');
+		newLink.style.cursor = 'pointer';
+		
+		newLink.setAttribute('href', '#section' + (i+1));
+		
+		//set a data-link attribute equals the section data-nav attr
+		newLink.setAttribute('data-link', sections[i].getAttribute('data-nav'));
+		
+	}
+		
+}
+
+
+/*
+ * End Main Functions
+*/
+
 // build the nav
+
+navbarFilling();
 
 
 // Add class 'active' to section when near top of viewport
 
+document.addEventListener('scroll', isSectionInViewport());
+
+
+function isSectionInViewport() {
+	
+	sections.forEach( (section) => {
+	
+	
+		if(window.scrollY >= section.offsetHeight) {
+		
+			
+			section.classList.add("your-active-class");
+			
+			
+		} else {
+		
+			
+			section.classList.remove("your-active-class");
+				
+		}	
+		
+	});
+	
+}
+// Scroll to section on link click
 
 // Scroll to anchor ID using scrollTO event
 
+const linksList = document.querySelectorAll("a[href^='#section']");
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
+linksList.forEach(function(link) {
+	
+	link.addEventListener('click', () => {
+		
+		linksList.forEach( (link) => {
+			
+			
+			if(link.classList.contains('active-link'))  {
+				
+				link.classList.remove('active-link');
+				
+			}
+			
+		});
+		
+		link.classList.add('active-link');
+		
+		
+		let ref = link.href.split('#section');
+		ref = '#section' + ref[1];
+		
 // Scroll to section on link click
-
-// Set sections as active
-
+		
+		window.scroll({
+			
+			behavior: 'smooth',
+			left: 0,
+			
+			top: document.querySelector(ref).offsetTop
+		});
+	});
+});
 
