dbbe5ee417b5f0dad77ef41a2954eb5dd3bf591a
Noha <33437197+Nohaxxxx@users.noreply.github.com>
2020-01-24 15:24:58 +0200

    editing on JS file

--- landing-page- Project 2/js/app.js
+++ landing-page- Project 2/js/app.js
@@ -18,16 +18,12 @@
  * 
 */
 
-const sections = document.querySelectorAll('[data-nav]');
-const navList = document.querySelector('#navbar__list');
 
-let newLink;
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
 
 
 
@@ -37,40 +33,14 @@ let newLink;
  * 
 */
 
-function navbarFilling() {
-	
-	
-	for(let i = 0; i < sections.length; i++) {
-		
-		
-		let navItem = document.createElement('LI');
-	
-		navList.append(navItem);
-	
-		newLink = document.createElement('A')
-	
-		navItem.append(newLink);
-		
-		newLink.innerHTML = 'section ' + (i+1);
-		
-		newLink.classList.add('menu__link');
-		newLink.style.cursor = 'pointer';
-		
-		newLink.setAttribute('href', '#section' + (i+1));
-		
-		
-	}
-		
-}
-
-
-
 // build the nav
-
-navbarFilling();
 
 
 // Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
 
 /**
  * End Main Functions
@@ -81,38 +51,7 @@ navbarFilling();
 // Build menu 
 
 // Scroll to section on link click
-// Scroll to anchor ID using scrollTO event
-
-const linksList = document.querySelectorAll("a[href^='#section']");
-
-linksList.forEach(function(link) {
-	
-	link.addEventListener('click', () => {
-		
-		linksList.forEach( (link) => {
-			
-			if(link.classList.contains('active-link'))  {
-				
-				link.classList.remove('active-link');
-				
-			}
-		});
-		
-		link.classList.add('active-link');
-		
-		
-		
-		let ref = link.href.split('#section');
-		ref = '#section' + ref[1];
-		
-		window.scroll({
-			
-			behavior: 'smooth',
-			left: 0,
-			
-			top: document.querySelector(ref).offsetTop
-		});
-	});
-});
 
 // Set sections as active
+
+
